e8e375b08ac5417da36a1e9f2011d6a636cb4467
Freiman Uribe <106196539+freiman-uribe@users.noreply.github.com>
2024-05-17 23:50:39 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -24,10 +24,10 @@ const App = () => {
 
         // Crear un nuevo Peer
         peerRef.current = new Peer(undefined, {
-          host: 'https://videoconferencia-freiman.netlify.app/', // Cambia esto a tu dirección IP o dominio del servidor PeerJS
+          host: 'videoconferencia-freiman.netlify.app', // Deja esto en blanco para que use el mismo host que el sitio web (Netlify)
           port: 9000,
           path: '/myapp',
-          secure: false // Cambia a true si estás usando HTTPS
+          secure: true // Cambia a true si estás usando HTTPS
         });
 
         // Manejar el evento 'open' para obtener el ID del peer y almacenarlo en el estado
